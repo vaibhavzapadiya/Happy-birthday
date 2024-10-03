bcd4c0f6b93e9a307bcf677e3760ced8524a8601
vaibhav zapadiya <137926275+vaibhavzapadiya@users.noreply.github.com>
2024-10-03 11:04:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,145 +1,54 @@
-const PI2 = Math.PI * 2;
-const random = (min, max) => Math.random() * (max - min + 1) + min | 0;
-const timestamp = () => new Date().getTime();
-
-class Firework {
-    constructor(x, y, targetX, targetY, shade, offsprings) {
-        this.dead = false;
-        this.offsprings = offsprings;
-        this.x = x;
-        this.y = y;
-        this.targetX = targetX;
-        this.targetY = targetY;
-        this.shade = shade;
-        this.history = [];
-        this.madeChilds = false;
-        
-    }
-    
-    update(delta, ctx, fireworks) {
-        if (this.dead) return;
-
-        let xDiff = this.targetX - this.x;
-        let yDiff = this.targetY - this.y;
-        if (Math.abs(xDiff) > 3 || Math.abs(yDiff) > 3) {
-            this.x += xDiff * 2 * delta;
-            this.y += yDiff * 2 * delta;
-            
-            this.history.push({ x: this.x, y: this.y });
-
-            if (this.history.length > 20) this.history.shift();
-
-        } else {
-            if (this.offsprings && !this.madeChilds) {
-                let babies = this.offsprings / 2;
-                for (let i = 0; i < babies; i++) {
-                    let targetX = this.x + this.offsprings * Math.cos(PI2 * i / babies) | 0;
-                    let targetY = this.y + this.offsprings * Math.sin(PI2 * i / babies) | 0;
-
-                    fireworks.push(new Firework(this.x, this.y, targetX, targetY, this.shade, 0));
-                }
-            }
-            this.madeChilds = true;
-            this.history.shift();
-        }
-
-        if (this.history.length === 0) this.dead = true;
-        else if (this.offsprings) {
-            for (let i = 0; i < this.history.length; i++) {
-                let point = this.history[i];
-                ctx.beginPath();
-                ctx.fillStyle = 'hsl(' + this.shade + ',100%,' + i + '%)';
-                ctx.arc(point.x, point.y, 1, 0, PI2, false);
-                ctx.fill();
-            }
-        } else {
-            ctx.beginPath();
-            ctx.fillStyle = 'hsl(' + this.shade + ',100%,50%)';
-            ctx.arc(this.x, this.y, 1, 0, PI2, false);
-            ctx.fill();
-        }
-    }
-}
-
-let canvas = document.getElementById('birthday');
-let ctx = canvas.getContext('2d');
-
-let then = timestamp();
-
-let fireworks = [];
-let counter = 0;
-
-
-
-const resize = () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-};
-
-const onClick = (evt) => {
-    let x = evt.clientX || evt.touches && evt.touches[0].pageX;
-    let y = evt.clientY || evt.touches && evt.touches[0].pageY;
-
-    let count = random(3, 5);
-    for (let i = 0; i < count; i++) {
-        fireworks.push(new Firework(
-            random(0, canvas.width),
-            canvas.height,
-            x,
-            y,
-            random(0, 260),
-            random(30, 110)
-        ));
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Shopping Cart</title>
+  <style>
+    .product, .cart-item {
+      display: flex;
+      justify-content: space-between;
+      padding: 10px;
+      margin: 10px 0;
+      border: 1px solid #ddd;
     }
 
-    counter = -1;
-};
-
-const update = (delta) => {
-    ctx.globalCompositeOperation = 'hard-light';
-    ctx.fillStyle = `rgba(20,20,20,${7 * delta})`;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    ctx.globalCompositeOperation = 'lighter';
-    for (let firework of fireworks) firework.update(delta, ctx, fireworks);
-
-    counter += delta * 3;
-    if (counter >= 1) {
-        fireworks.push(new Firework(
-            random(0, canvas.width),
-            canvas.height,
-            random(0, canvas.width),
-            random(0, canvas.height),
-            random(0, 360),
-            random(30, 110)
-            
-        ));
-        counter = 0;
+    .product-list, .cart-list {
+      width: 45%;
+      float: left;
+      margin-right: 5%;
     }
 
-    if (fireworks.length > 1000) {
-        fireworks = fireworks.filter(firework => !firework.dead);
+    .cart-list {
+      float: right;
     }
-    
-};
 
-const animate = () => {
-    requestAnimationFrame(animate);
+    .product img, .cart-item img {
+      width: 100px;
+    }
 
-    let now = timestamp();
-    let delta = now - then;
-    then = now;
-    update(delta / 1000);
-    
-};
+    .controls {
+      display: flex;
+      align-items: center;
+    }
 
-// Event listeners
-window.addEventListener('resize', resize);
-document.addEventListener('click', onClick);
-document.addEventListener('touchstart', onClick);
+    button {
+      margin: 0 5px;
+    }
+  </style>
+</head>
+<body>
 
-// Button to trigger celebration
-document.getElementById('celebrateButton')?.addEventListener('click', onClick);
+  <!-- Products List -->
+  <div class="product-list" id="productList"></div>
 
-resize();
-animate();
+  <!-- Cart List -->
+  <div class="cart-list" id="cartList">
+    <h3>Your Cart</h3>
+    <div id="cartItems"></div>
+    <h4>Total: $<span id="TotalPrice">0.00</span></h4>
+  </div>
+
+  <script src="cart.js"></script>
+</body>
+</html>
